3cba6d3e0e351da8e1ab18f4e4b834e46bf596a7
unknown <paul-emmanuel.buffe@laplateforme.io>
2025-03-05 11:27:17 +0100

    upgrade display

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -6,10 +6,17 @@ def index_case_selectionnee(valeur, matrice):
                 return (x, y)
     return None
 
+# Fonction pour afficher la grille
+def afficher_grille(myDico):
+    print("\nGrille de jeu :")
+    for ligne in myDico:
+        print("|".join(ligne))
+        print("-" * 5)
+
 # Fonction pour les tours de jeu
 def tour(joueur, symbole, myDico, liste):
     while True:
-        choix_case = input(f"{joueur}, donnez un numéro de case : ")
+        choix_case = input(f"\n{joueur}, donnez un numéro de case (1-9) : ")
 
         if choix_case not in liste:
             print("La valeur renseignée n'est pas valide, essayez à nouveau.")
@@ -21,26 +28,23 @@ def tour(joueur, symbole, myDico, liste):
             liste.remove(choix_case)
             break
 
-    # Impression de la liste myDico modifiée
-    for ligne in myDico:
-        print(ligne)
-
+    afficher_grille(myDico)
     return choix_victoire(joueur, myDico)
 
 # Fonction déterminant les conditions de victoire
 def choix_victoire(joueur, myDico):
     for ligne in myDico:
         if ligne[0] == ligne[1] == ligne[2] and ligne[0] != " ":
-            print(f"{joueur}, vous avez gagné !! L'autre joueur paye l'apéro.")
+            print(f"\n{joueur}, vous avez gagné !! L'autre joueur paye l'apéro.")
             return True
 
     for colonne in range(3):
         if myDico[0][colonne] == myDico[1][colonne] == myDico[2][colonne] and myDico[0][colonne] != " ":
-            print(f"{joueur}, vous avez gagné !! L'autre joueur paye l'apéro.")
+            print(f"\n{joueur}, vous avez gagné !! L'autre joueur paye l'apéro.")
             return True
 
     if (myDico[0][0] == myDico[1][1] == myDico[2][2] and myDico[0][0] != " ") or (myDico[0][2] == myDico[1][1] == myDico[2][0] and myDico[0][2] != " "):
-        print(f"{joueur}, vous avez gagné !! L'autre joueur paye l'apéro.")
+        print(f"\n{joueur}, vous avez gagné !! L'autre joueur paye l'apéro.")
         return True
 
     return False
@@ -50,33 +54,21 @@ def morpion():
     myDico = [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"]]
     liste = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
+    afficher_grille(myDico)
+
     while True:
         if tour("Joueur 1", "X", myDico, liste):
             break
         if tour("Joueur 2", "O", myDico, liste):
             break
         if not liste:
-            print("Match nul !")
+            print("\nMatch nul !")
             break
-
-# Variables
-myDico = [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"]]
-liste = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-
-for ligne in myDico:
-    print(ligne)
-
-morpion()
 
 # Boucle de redémarrage
 while True:
-    relancer_partie = input("Nouvelle partie ? (O/N) : ")
+    morpion()
+    relancer_partie = input("\nNouvelle partie ? (O/N) : ")
     if relancer_partie.upper() == "N":
         print("À bientôt !")
         break
-    else:
-        myDico = [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"]]
-        liste = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-        for ligne in myDico:
-            print(ligne)
-        morpion()
